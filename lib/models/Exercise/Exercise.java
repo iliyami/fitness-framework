269cd4034d0db2e4616283e311e95ebb3462ab2b
IliyaMi <iliya.mi78@gmail.com>
2023-01-03 11:18:46 +0330

    Develop exercise model class

---
+++ lib/models/Exercise/Exercise.java
@@ -0,0 +1,49 @@
+package Exercise;
+/**
+ * Exercise
+ */
+public class Exercise {
+    private int id;
+    private String name;
+    private String instruction;
+    private Integer popularity;
+    private ExerciseType exerciseType;
+
+    public Exercise(int id, String name, String instruction, int popularity, ExerciseType exerciseType) {
+        this.id = id;
+        this.name = name;
+        this.instruction = instruction;
+        this.popularity = popularity;
+        this.exerciseType = exerciseType;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getInstruction() {
+        return instruction;
+    }
+    public void setInstruction(String instruction) {
+        this.instruction = instruction;
+    }
+    public int getPopularity() {
+        return popularity;
+    }
+    public void setPopularity(int popularity) {
+        this.popularity = popularity;
+    }
+    public ExerciseType getExerciseType() {
+        return exerciseType;
+    }
+    public void setExerciseType(ExerciseType exerciseType) {
+        this.exerciseType = exerciseType;
+    }
+    public boolean updateExercise(int id, String name, String instruction, int popularity, ExerciseType exerciseType) {
+        return true;
+    }
+}

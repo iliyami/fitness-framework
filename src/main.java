dd330bd0bb403a67c89f2f300fe61b108f8170fc
IliyaMi <iliya.mi78@gmail.com>
2023-01-03 11:18:23 +0330

    Fix typo

--- src/main.java
+++ src/main.java
@@ -1,5 +1,4 @@
-// A Java program to print GeeksforGeeks
-public class Run {
+public class Main {
 	public static void main(String args[])
 	{
 		System.out.println("The program start running ...");

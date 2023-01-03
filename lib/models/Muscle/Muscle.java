2f3c05c8ff6bbec34a8f3650d7166679d3a0a65a
IliyaMi <iliya.mi78@gmail.com>
2023-01-03 11:22:59 +0330

    Develop the muscle model

---
+++ lib/models/Muscle/Muscle.java
@@ -0,0 +1,22 @@
+package Muscle;
+
+public class Muscle {
+    private String name;
+    private BodyPart bodyPart;
+    public Muscle(String name, BodyPart bodyPart) {
+        this.name = name;
+        this.bodyPart = bodyPart;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public BodyPart getBodyPart() {
+        return bodyPart;
+    }
+    public void setBodyPart(BodyPart bodyPart) {
+        this.bodyPart = bodyPart;
+    }
+}

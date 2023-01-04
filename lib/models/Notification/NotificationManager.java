b5111caae3f5d0cd22ceac4242cc39cb19d015e4
Afshin Zanganeh <afshinzanganeh2001@gmail.com>
2023-01-04 08:05:09 +0330

    fear: add NotificationManager class

---
+++ lib/models/Notification/NotificationManager.java
@@ -0,0 +1,19 @@
+package NotificationManager;
+
+import java.util.List;
+
+public class NotificationManager {
+    private List<Notification> subscribers;
+
+    public void subscribers(Notification notification) {
+        return;
+    }
+
+    public void unsubscribe(int notificationId) {
+        return;
+    }
+
+    public void notifySubscribers() {
+        return;
+    }
+}

587dddc11e8c0c349da311030d5e2d9d02d9e293
Iliya Mirzaei <iliya.mi78@gmail.com>
2023-01-24 02:28:09 +0330

    Recreate main to fix the nerving typo issue

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+import CLI.CLIParser;
+
+/* Main Program */
+public class Main {
+	public static void main(String args[]) throws IOException
+	{
+		CLIParser.buildMenu();		
+	}
+}

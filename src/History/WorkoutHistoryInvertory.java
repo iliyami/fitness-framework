3342c2dec6b88bfeca4d7e479c2e9e30c4670894
Iliya Mirzaei <iliya.mi78@gmail.com>
2023-01-24 04:31:58 +0330

    Comment out the file

--- src/History/WorkoutHistoryInvertory.java
+++ src/History/WorkoutHistoryInvertory.java
@@ -1,47 +1,50 @@
-package History;
+/* This singleton class is additional */
+/* TODO should remove this file */
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+// package History;
 
-// using singleton
-public class WorkoutHistoryInvertory {
-    private static WorkoutHistoryInvertory singleWorkoutHistoryInvertory = null;
-    private List<WorkoutHistory> workoutHistories = new ArrayList<WorkoutHistory>();
+// import java.util.ArrayList;
+// import java.util.Iterator;
+// import java.util.List;
+
+// // using singleton
+// public class WorkoutHistoryInvertory {
+//     private static WorkoutHistoryInvertory singleWorkoutHistoryInvertory = null;
+//     private List<WorkoutHistory> workoutHistories = new ArrayList<WorkoutHistory>();
   
-    public static WorkoutHistoryInvertory getInstance()
-    {
-        if (singleWorkoutHistoryInvertory == null){
-            singleWorkoutHistoryInvertory = new WorkoutHistoryInvertory();
-        }
-        return singleWorkoutHistoryInvertory;
-    }
+//     public static WorkoutHistoryInvertory getInstance()
+//     {
+//         if (singleWorkoutHistoryInvertory == null){
+//             singleWorkoutHistoryInvertory = new WorkoutHistoryInvertory();
+//         }
+//         return singleWorkoutHistoryInvertory;
+//     }
 
-    public WorkoutHistory getWorkoutHistoryById(int id){
-        Iterator<WorkoutHistory> iterator = this.workoutHistories.iterator();
-        while (iterator.hasNext()) {
-            WorkoutHistory workoutHistory = iterator.next();
-            if (workoutHistory.getId() == id) {
-                return workoutHistory;
-            }
-        }
-        return null;
-    }
+//     public WorkoutHistory getWorkoutHistoryById(int id){
+//         Iterator<WorkoutHistory> iterator = this.workoutHistories.iterator();
+//         while (iterator.hasNext()) {
+//             WorkoutHistory workoutHistory = iterator.next();
+//             if (workoutHistory.getId() == id) {
+//                 return workoutHistory;
+//             }
+//         }
+//         return null;
+//     }
 
-    public void addWorkoutHistory(WorkoutHistory workoutHistory){
-        WorkoutHistory existedWorkoutHistory = getWorkoutHistoryById(workoutHistory.getId());
-        if (existedWorkoutHistory == null)
-            this.workoutHistories.add(workoutHistory); 
-    }
+//     public void addWorkoutHistory(WorkoutHistory workoutHistory){
+//         WorkoutHistory existedWorkoutHistory = getWorkoutHistoryById(workoutHistory.getId());
+//         if (existedWorkoutHistory == null)
+//             this.workoutHistories.add(workoutHistory); 
+//     }
 
-    public void updateWorkoutHistory(WorkoutHistory workoutHistory){
-        WorkoutHistory existedWorkoutHistory = getWorkoutHistoryById(workoutHistory.getId());
-        if(existedWorkoutHistory != null)
-            deleteWorkoutHistory(existedWorkoutHistory);
-        this.workoutHistories.add(workoutHistory);
-    }
+//     public void updateWorkoutHistory(WorkoutHistory workoutHistory){
+//         WorkoutHistory existedWorkoutHistory = getWorkoutHistoryById(workoutHistory.getId());
+//         if(existedWorkoutHistory != null)
+//             deleteWorkoutHistory(existedWorkoutHistory);
+//         this.workoutHistories.add(workoutHistory);
+//     }
 
-    public void deleteWorkoutHistory(WorkoutHistory workoutHistory){
-        workoutHistories.remove(workoutHistory);
-    }
-}
+//     public void deleteWorkoutHistory(WorkoutHistory workoutHistory){
+//         workoutHistories.remove(workoutHistory);
+//     }
+// }

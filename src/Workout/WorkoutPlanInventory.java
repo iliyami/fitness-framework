a808a219d8bcdf9c74a33713557d0c201d80506e
Iliya Mirzaei <iliya.mi78@gmail.com>
2023-01-24 04:29:44 +0330

    Comment out the file

--- src/Workout/WorkoutPlanInventory.java
+++ src/Workout/WorkoutPlanInventory.java
@@ -1,49 +1,51 @@
+/* This singleton class is additional */
+/* TODO should remove this file */
 
-package Workout;
+// package Workout;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+// import java.util.ArrayList;
+// import java.util.Iterator;
+// import java.util.List;
 
 
-// using singleton
-public class  WorkoutPlanInventory {
-    private static WorkoutPlanInventory singleWorkoutPlanInventory = null;
-    private List<WorkoutPlan> workoutPlans= new ArrayList<WorkoutPlan>();
+// // using singleton
+// public class  WorkoutPlanInventory {
+//     private static WorkoutPlanInventory singleWorkoutPlanInventory = null;
+//     private List<WorkoutPlan> workoutPlans= new ArrayList<WorkoutPlan>();
   
-    public static WorkoutPlanInventory getInstance()
-    {
-        if (singleWorkoutPlanInventory == null) {
-            singleWorkoutPlanInventory = new WorkoutPlanInventory();
-        }
-        return singleWorkoutPlanInventory;
-    }
+//     public static WorkoutPlanInventory getInstance()
+//     {
+//         if (singleWorkoutPlanInventory == null) {
+//             singleWorkoutPlanInventory = new WorkoutPlanInventory();
+//         }
+//         return singleWorkoutPlanInventory;
+//     }
 
-    public WorkoutPlan getWorkoutPlanById(int id){
-        Iterator<WorkoutPlan> iterator = this.workoutPlans.iterator();
-        while (iterator.hasNext()) {
-            WorkoutPlan workoutPlan = iterator.next();
-            if (workoutPlan.getId() == id) {
-                return workoutPlan;
-            }
-        }
-        return null;
-    }
+//     public WorkoutPlan getWorkoutPlanById(int id){
+//         Iterator<WorkoutPlan> iterator = this.workoutPlans.iterator();
+//         while (iterator.hasNext()) {
+//             WorkoutPlan workoutPlan = iterator.next();
+//             if (workoutPlan.getId() == id) {
+//                 return workoutPlan;
+//             }
+//         }
+//         return null;
+//     }
 
-    public void addWorkoutPlan(WorkoutPlan workoutPlan){
-        WorkoutPlan existedWorkoutPlan = getWorkoutPlanById(workoutPlan.getId());
-        if (existedWorkoutPlan == null)
-            this.workoutPlans.add(workoutPlan); 
-    }
+//     public void addWorkoutPlan(WorkoutPlan workoutPlan){
+//         WorkoutPlan existedWorkoutPlan = getWorkoutPlanById(workoutPlan.getId());
+//         if (existedWorkoutPlan == null)
+//             this.workoutPlans.add(workoutPlan); 
+//     }
 
-    public void UpdateWorkoutPlan(WorkoutPlan workoutPlan){
-        WorkoutPlan existedWorkoutPlan = getWorkoutPlanById(workoutPlan.getId());
-        if(existedWorkoutPlan != null)
-            deleteWorkoutPlan(existedWorkoutPlan);
-        this.workoutPlans.add(workoutPlan);
-    }
+//     public void UpdateWorkoutPlan(WorkoutPlan workoutPlan){
+//         WorkoutPlan existedWorkoutPlan = getWorkoutPlanById(workoutPlan.getId());
+//         if(existedWorkoutPlan != null)
+//             deleteWorkoutPlan(existedWorkoutPlan);
+//         this.workoutPlans.add(workoutPlan);
+//     }
 
-    public void deleteWorkoutPlan(WorkoutPlan workoutPlan){
-        workoutPlans.remove(workoutPlan);
-    }
-}
+//     public void deleteWorkoutPlan(WorkoutPlan workoutPlan){
+//         workoutPlans.remove(workoutPlan);
+//     }
+// }

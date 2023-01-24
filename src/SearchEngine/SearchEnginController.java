94e53e07adfa474f228d1f298653dc7aa9f3a38d
Iliya Mirzaei <iliya.mi78@gmail.com>
2023-01-24 17:05:28 +0330

    Add searchEngineControllerTests test

--- src/SearchEngine/SearchEnginController.java
+++ src/SearchEngine/SearchEnginController.java
@@ -5,6 +5,20 @@ public class SearchEnginController {
     private SearchExerciseController searchExerciseController;
     private SearchWorkoutHistoryController searchWorkoutHistoryController;
 
+    public void setSearchExerciseController(SearchExerciseController searchExerciseController) {
+        this.searchExerciseController = searchExerciseController;
+    }
+
+    public SearchEnginController() {
+        setSearchExerciseController(new SearchExerciseController());
+        setSearchWorkoutHistoryController(new SearchWorkoutHistoryController());
+    }
+
+
+    public void setSearchWorkoutHistoryController(SearchWorkoutHistoryController searchWorkoutHistoryController) {
+        this.searchWorkoutHistoryController = searchWorkoutHistoryController;
+    }
+
     public SearchExerciseController getSearchExerciseController() {
         return searchExerciseController;
     }
--- test/SearchEngine/SearchEngineTest.java
+++ test/SearchEngine/SearchEngineTest.java
@@ -4,7 +4,19 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import SearchEngine.SearchEnginController;
+
 
 public class SearchEngineTest {
+    public SearchEngineTest() {
+        mockController = new SearchEnginController();
+    }
 
+    private SearchEnginController mockController;
+
+    @Test
+    public void testControllers() {
+        assertNotEquals(null, mockController.getSearchExerciseController());
+        assertNotEquals(null, mockController.getSearchWorkoutHistoryController());
+    }
 }
